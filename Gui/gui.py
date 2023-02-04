7a5c05756e57d010c4e642a2241c53e75af961f7
La1xuan <bilibililaixuan@gmail.com>
2023-02-04 08:25:34 -0500

    update interface to be a text shape

--- Gui/gui.py
+++ Gui/gui.py
@@ -5,33 +5,27 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 import time
 
-def processData():
-    print("First Name: %s\nLast Name: %s" % (e1.get(), e2.get()))
+def processData(event=None):
+    print("First Name: %s\nLast Name: " % (e1.get()))
 
 gui = tk.Tk()
-gui.geometry("500x800")
+gui.geometry("500x500")
 style = ttk.Style("minty")
 
-tk.Label(gui, text="First Name").grid(row=0)
-tk.Label(gui, text="Last Name").grid(row=1)
+#tk.Label(gui, text="First Name").grid(row=0)
 
-e1 = tk.Entry(gui)
-e2 = tk.Entry(gui)
+e1 = tk.Entry(gui, width=50)
+e1.insert(0, "Please describe your symptom")
 
 e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
+e1.place(x=115, y=455)
 
-tk.Button(gui, 
-          text='Quit', 
-          command=gui.quit).grid(row=3, 
-                                    column=0, 
-                                    sticky=tk.W, 
-                                    pady=4)
-b1 = ttk.Button(gui, text="Button 1", bootstyle=SUCCESS)
-tk.Button(gui, text='Show', command=processData).grid(row=3, 
-                                                               column=10, 
-                                                               sticky=tk.W, 
-                                                               pady=4)
+quitButton = ttk.Button(gui, text="End Chat", bootstyle=(INFO, OUTLINE), command=gui.quit)
+quitButton.place(x=25, y=450)
 
+sentButton = ttk.Button(gui, text="|>", bootstyle=(SUCCESS, OUTLINE), command=processData)
+sentButton.place(x=440, y=450)
+
+gui.bind('<Return>', processData)
 
 gui.mainloop()
